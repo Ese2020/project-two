9f42cf81035a79d1b6d2a4a72532e8e75e0dcde8
Ese Paive <89306416+Ese2020@users.noreply.github.com>
2022-05-15 02:14:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-git
+let hamDropDown = document.querySelector(".ham-dropdown");
+let body = document.getElementsById("body")
+
+function myFunction(x) {
+    x.classList.toggle("change");
+    hamDropDown.classList.toggle("d-none");
+    hamDropDown.style.opacity = 0.7;
+    body.style.backgroundColor = 'black';
+}
